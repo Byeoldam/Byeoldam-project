7e1777658df8ba0bb267900f448e1ac63b972952
donghyeon <inews2432@gmail.com>
2025-02-15 15:57:11 +0900

    extension cors 추가

--- src/main/java/com/be/byeoldam/config/WebConfig.java
+++ src/main/java/com/be/byeoldam/config/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://3.35.55.187:5173","http://frontend:3000","http://frontend:5173","http://127.0.0.1:8080", "http://localhost:5173", "http://localhost:5174") // Swagger UI와 관련된 Origin 허용
+                .allowedOriginPatterns("chrome-extension://*") // 익스텐션 프로토콜로 시작하는 모든 origin에서의 요청을 허용
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 //.allowCredentials(true); // 인증 정보(쿠키 등) 허용
