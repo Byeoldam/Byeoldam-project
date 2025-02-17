97f497e48135c8fbd44ccdfeb0e176a6d414f0cb
eenzzi <purelej99@gmail.com>
2025-02-17 14:19:13 +0900

    공유  컬렉션 페이지 색 조정

--- vue-app/src/views/SharedView.vue
+++ vue-app/src/views/SharedView.vue
@@ -126,6 +126,7 @@ const createNewCollection = () => {
     display: flex;
     flex-direction: column;
     min-height: 100vh;
+    background-color: #F5F5F5;
 }
 
 .header {
@@ -177,7 +178,7 @@ const createNewCollection = () => {
 .top-section {
     position: sticky;
     top: 0;
-    background: white;
+    background: #F5F5F5;
     padding: 10px 0;
     z-index: 98;
     display: flex;
@@ -207,9 +208,9 @@ const createNewCollection = () => {
 }
 
 .filter-btn.active {
-    background: #007bff;
-    color: white;
-    border-color: #007bff;
+    background: #3730A3;
+    color: rgba(255, 255, 255, 0.901);
+    border-color: #3730A3;
 }
 
 .new-collection-btn {
@@ -219,15 +220,15 @@ const createNewCollection = () => {
     padding: 8px 16px;
     border: none;
     border-radius: 20px;
-    background: #007bff;
-    color: white;
+    background: #3730A3;
+    color: rgba(255, 255, 255, 0.901);
     cursor: pointer;
     transition: all 0.3s ease;
     font-weight: 500;
 }
 
 .new-collection-btn:hover {
-    background: #0056b3;
+    background: #3830a3c5;
 }
 
 .plus-icon {
@@ -260,7 +261,7 @@ const createNewCollection = () => {
 }
 
 .page-header {
-    background: linear-gradient(to right, #f8f9fa, #ffffff);
+    /* background: linear-gradient(to right, #f8f9fa, #ffffff); */
     padding: 16px 24px;
     border-radius: 12px;
     margin-bottom: 24px;
@@ -279,7 +280,7 @@ const createNewCollection = () => {
 
 .title-icon {
     font-size: 1.5rem;
-    color: #007bff;
+    color: #3730A3;
 }
 
 .title {
