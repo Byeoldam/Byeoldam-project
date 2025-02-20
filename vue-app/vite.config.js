cd02386f48419f607c5f0ab0e9f373bdfc9647b4
donghyeon <inews2432@gmail.com>
2025-02-20 12:35:19 +0900

    화면 하단 vue

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',  // Docker에서 접근 가능하도록 설정
-    port: 5173,       // 컨테이너에서 사용할 포트
+    port: 5174,       // 컨테이너에서 사용할 포트
     strictPort: true, // 고정된 포트 사용
     watch: {
       usePolling: true, // 파일 변경 감지를 위한 설정 (Docker 환경에서 필요할 수 있음)
