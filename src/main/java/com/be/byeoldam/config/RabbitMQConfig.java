99c752e6f484fa906cff19c3db38414d6d6b60cf
ucs21 <usad63mbn@gmail.com>
2025-06-19 16:51:59 +0900

    feat: RabbitMQ-Publish-Confirm

--- src/main/java/com/be/byeoldam/config/RabbitMQConfig.java
+++ src/main/java/com/be/byeoldam/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.be.byeoldam.config;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -7,12 +10,23 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class RabbitMQConfig {
 
     private static final String QUEUE_NAME = "notification.queue";
     private static final String EXCHANGE_NAME = "notification.exchange";
     private static final String ROUTING_KEY = "notification.key";
+
+    private static final Logger log = LoggerFactory.getLogger(RabbitMQConfig.class);
+
+    // Message가 Queue에 도착했는지 확인하기 위한 변수
+    public static final Map<String, CompletableFuture<Boolean>> returnFutureMap = new ConcurrentHashMap<>();
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
@@ -38,6 +52,43 @@ public class RabbitMQConfig {
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            String correlationId = correlationData != null ? correlationData.getId() : "NULL";
+            CompletableFuture<Boolean> future = RabbitMQConfig.returnFutureMap.get(correlationId);
+
+            if(ack){
+                log.info("[ConfirmCallback] 메시지 전송 성공, correlationId = {}", correlationId);
+                Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+                    if (!future.isDone()) {
+                        future.complete(true);
+                    }
+                }, 300, TimeUnit.MILLISECONDS);
+            }else{
+                log.error("[ConfirmCallback] 메시지 전송 실패, correlationId = {}", correlationId);
+                log.error("원인: {}", cause);
+
+                // Message 전송 실패 시 필요한 추가 로직은 여기에 작성하기.
+                future.complete(false);
+            }
+
+            RabbitMQConfig.returnFutureMap.remove(correlationId);
+        });
+        
+        // 해당 메소드는 라우팅 실패 시에만 호출되기 때문에 false를 저장
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setReturnsCallback(returnedMessage -> {
+            MessageProperties props = returnedMessage.getMessage().getMessageProperties();
+            String correlationId = props != null ? props.getCorrelationId() : null;
+            CompletableFuture<Boolean> future = RabbitMQConfig.returnFutureMap.get(correlationId);
+
+            if (future != null) {
+                future.complete(false);
+                RabbitMQConfig.returnFutureMap.remove(correlationId);
+            }
+            log.error("[ReturnCallback] 메시지 전송 실패, correlationId = {}", correlationId);
+        });
+
         return rabbitTemplate;
     }
 
--- src/main/java/com/be/byeoldam/domain/notification/event/NotificationProducer.java
+++ src/main/java/com/be/byeoldam/domain/notification/event/NotificationProducer.java
@@ -1,9 +1,15 @@
 package com.be.byeoldam.domain.notification.event;
 
+import com.be.byeoldam.config.RabbitMQConfig;
 import com.be.byeoldam.domain.notification.dto.NotificationMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -14,6 +20,30 @@ public class NotificationProducer {
     private static final String ROUTING_KEY = "notification.key";
 
     public void sendNotification(NotificationMessage message) {
-        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message);
+        String correlationId = UUID.randomUUID().toString();
+
+        // 라우팅 성공 여부를 저장
+        CompletableFuture<Boolean> returnFuture = new CompletableFuture<>();
+        RabbitMQConfig.returnFutureMap.put(correlationId, returnFuture);
+
+        CorrelationData correlationData = new CorrelationData(correlationId);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, message, msg -> {
+            msg.getMessageProperties().setCorrelationId(correlationData.getId());
+            return msg;
+        },correlationData);
+
+       try {
+           boolean isAck = correlationData.getFuture().get(5, TimeUnit.SECONDS).isAck();
+           if (!isAck) {
+               throw new RuntimeException("Broker 전달 실패");
+           }
+
+           boolean isReturned = returnFuture.get(5, TimeUnit.SECONDS);
+           if (!isReturned) { 
+               throw new RuntimeException("Routing 실패");
+           }
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
     }
 }
